2ff846066eb42c82cab71fc729e03360b34cbb2a
Jackylai01 <94669444+Jackylai01@users.noreply.github.com>
2023-02-05 22:44:52 +0800

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -11,7 +11,7 @@ const Index = ({ orders, products }) => {
   const handleDelete = async (id) => {
     try {
       const res = await axios.delete(
-        "http://localhost:3000/api/products/" + id
+        "https://next-cake-ecommerce.vercel.app/api/products/" + id
       );
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
     } catch (err) {
@@ -24,7 +24,7 @@ const Index = ({ orders, products }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
+      const res = await axios.put("https://next-cake-ecommerce.vercel.app/api/orders/" + id, {
         status: currentStatus + 1,
       });
       setOrderList([
@@ -128,8 +128,8 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const productRes = await axios.get("http://localhost:3000/api/products");
-  const orderRes = await axios.get("http://localhost:3000/api/orders");
+  const productRes = await axios.get("https://next-cake-ecommerce.vercel.app/api/products");
+  const orderRes = await axios.get("https://next-cake-ecommerce.vercel.app/api/orders");
 
   return {
     props: {
