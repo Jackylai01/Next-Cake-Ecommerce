a06a8ef731a82ad029bd5c6021ea2d776fd64d8e
Jackylai01 <94669444+Jackylai01@users.noreply.github.com>
2023-02-05 22:40:08 +0800

    Update index.js

--- pages/searchOrder/index.js
+++ pages/searchOrder/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "React";
+import React, { useState } from "react";
 import styles from "../../styles/searchOrder.module.css";
 import { useRouter } from "next/router";
 
